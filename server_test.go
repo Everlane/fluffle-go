7b4436d07190c473589b23cf9123bf6294d5d86a
Dirk Gadsden <dirk@esherido.com>
2016-10-15 12:16:11 -0700

    Add basic server test

---
+++ server_test.go
@@ -0,0 +1,40 @@
+package fluffle
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBasicServer(t *testing.T) {
+	queue := "test.1"
+
+	server := NewServer()
+	err := server.Connect("amqp://localhost")
+	require.Nil(t, err)
+
+	server.DrainFunc(queue, func(req *Request) (interface{}, error) {
+		params := make([]string, len(req.Params))
+		for i, param := range req.Params {
+			params[i] = fmt.Sprintf("%#v", param)
+		}
+		resp := fmt.Sprintf("%v(%v)", req.Method, strings.Join(params, ", "))
+		return resp, nil
+	})
+
+	go func(){
+		err := server.Start()
+		require.Nil(t, err)
+	}()
+
+	client, err := NewClient("amqp://localhost")
+	require.Nil(t, err)
+
+	resp, err := client.Call("foo", []interface{}{"bar"}, queue)
+	require.Nil(t, err)
+	require.Equal(t, resp, `foo("bar")`)
+
+	server.Stop()
+}
